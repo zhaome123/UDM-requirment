f1f1ae00c29dd16529d267364599cfb74a3f3d01
Yanbo Deng <dengya@ececs.udmercy.edu>
2020-01-30 19:28:06 -0500

    Update project_20200116_2_.ino

--- project_20200116_2_.ino
+++ project_20200116_2_.ino
@@ -8,8 +8,8 @@ Pixy2 pixy;
 #define PWMA 4
 #define PWMB 5
 #define A A0
-int s1 = 70, s2 = 60;
-int  Mx, mx1, mx2, my1, my2, Area, w, h, i, countTurn = 0;
+int s1 = 59, s2 = 42;
+int  Mx, mx1, mx2, my1, my2,Area1, Area2, w1, w2, h1, h2, D,countTurn = 0, Di,vector;
 boolean Flag = 0;
 void setup()
 {
@@ -29,30 +29,78 @@ void setup()
   digitalWrite(AIN2, HIGH);
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, HIGH);
-  analogWrite(PWMA, s1);
-  analogWrite(PWMB, s2);
+//  speedControl('G', 0, 0);
 
+//pixy.changeProg("color");
+//-----------------------------color
+
+  while (Flag == 0)
+  { 
+    pixy.ccc.getBlocks();
+    voidProject();
+    if (pixy.ccc.numBlocks = 1)
+    {
+      Serial.print("one block");
+      //---------------------------------------------------when only find one block
+      OneBlockFollow();
+//      pixy.changeProg("line");
+    }
+    else if (pixy.ccc.numBlocks > 1)
+    {
+      Serial.print("two block");
+      TwoBlockFollow();
+//      pixy.changeProg("line");
+    }
+    else
+    {
+      if (countTurn < 100) //---------------------------------------------need test turn speed
+      {
+        speedControl('o', 59, 0); //------------------------------------------
+        delay(30);
+        countTurn = countTurn + 10;
+        
+      }
+      else
+      {
+        speedControl('R', 0, 0); //-----------------------------------------------------------
+       
+        delay(70);
+      }
+    }
+
+  }
+
+
+
+
+
+  //-----------------------------color
 }
 void speedControl(char ControlWord, int Speed1, int Speed2)
 {
   switch (ControlWord)
-  case 'G':
-  { analogWrite(PWMA, 59);
-    analogWrite(PWMB, 42);
-  };
-case 'L':
-  { analogWrite(PWMA, 37);
-    analogWrite(PWMB, 46);
-  };
-case 'R':
-  {
-    analogWrite(PWMA, 63);
-    analogWrite(PWMB, 36);
-  };
-default:
-  {
-    analogWrite(PWMA, Speed1);
-    analogWrite(PWMB, Speed2);
+  { case 'G':
+       analogWrite(PWMA, 59);
+        analogWrite(PWMB, 46);
+        break;
+      
+    case 'L':
+       analogWrite(PWMA, 37);
+        analogWrite(PWMB, 52);
+        break;
+      
+    case 'R':
+      
+        analogWrite(PWMA, 63);
+        analogWrite(PWMB, 45);
+        break;
+      
+    default:
+      
+        analogWrite(PWMA, Speed1);
+        analogWrite(PWMB, Speed2);
+        break;
+      
   }
 }
 void voidProject()//------------------------------------------------------------------------------
@@ -60,7 +108,7 @@ void voidProject()//------------------------------------------------------------
   Serial.println(Di);
   if (Di <= 10)
   {
-    speedControl('STOP',0,0);
+    speedControl('STOP', 0, 0);
     while (Di <= 10)
     { Di = analogRead (A);
       delay(30);
@@ -68,51 +116,47 @@ void voidProject()//------------------------------------------------------------
   }
   else if (Di <= 24 )
   {
-    speedControl('Restart',55,38);//-----------------------------------------------------------------
+    speedControl('Restart', 55, 38); //-----------------------------------------------------------------
   }
 }
 
 void OneBlockFollow()
 {
-
+  voidProject();
   Mx = pixy.ccc.blocks->m_x;
   Serial.println("only one block");
   Serial.println(Mx);
-  w = pixy.ccc.blocks->m_width;
-  h = pixy.ccc.blocks->m_height;
-  Area = w * h;
+  w1 = pixy.ccc.blocks->m_width;
+  h1 = pixy.ccc.blocks->m_height;
+  Area1 = w1 * h1;
   Serial.println("width height area");
-  Serial.println(w);
-  Serial.println(h);
-  Serial.println(Area);
-  if (Area >= 9000) {
-   
+  Serial.println(w1);
+  Serial.println(h1);
+  Serial.println(Area1);
+  if (Area1 >= 7000) {
+
     pixy.changeProg("line");
-    digitalWrite(AIN1, LOW);
-    digitalWrite(AIN2, HIGH);
-    digitalWrite(BIN1, LOW);
-    digitalWrite(BIN2, HIGH);
-    analogWrite(PWMA, s1);
-    analogWrite(PWMB, s2);
+    speedControl('G', 0, 0);
     Flag = 1;
   }
   else if (Mx <= 105) {
-    TL();
+    speedControl('L', 0, 0);
     delay(70);
   }
   else if (Mx >= 210)
   {
-    TR();
+    speedControl('R', 0, 0);
     delay(70);
   }
   else {
-    GS();
+    speedControl('G', 0, 0);
     delay(70);
   }
 }
+//----------------------------------------------------------------------------20200128
 void TwoBlockFollow()
 {
-
+  voidProject();
   mx1 = pixy.ccc.blocks[0].m_x;
   my1 = pixy.ccc.blocks[0].m_y;
 
@@ -120,18 +164,28 @@ void TwoBlockFollow()
   my2 = pixy.ccc.blocks[1].m_y;
 
 
+  w1 = pixy.ccc.blocks[0].m_width;
+  h1 = pixy.ccc.blocks[0].m_height;
+  Area1 = w1 * h1;
+  Serial.println("width height area");
+  Serial.println(w1);
+  Serial.println(h1);
+  Serial.println(Area1);
+  w2 = pixy.ccc.blocks[1].m_width;
+  h2 = pixy.ccc.blocks[1].m_height;
+  Area2 = w2 * h2;
+  Serial.println("width height area");
+  Serial.println(w2);
+  Serial.println(h2);
+  Serial.println(Area2);
+  D = abs(my2 - my1);
   //compare block position
   if (mx1 > mx2)
   {
-    if ((mx2 < 105) && (mx1 > 210) && (my1 >= 81) && (my1 <= 150) && (my2 >= 81) && (my2 <= 150))
+     if (D < 10 && Area2 > 400 && Area1 > 400)
     {
       pixy.changeProg("line");
-      digitalWrite(AIN1, LOW);
-      digitalWrite(AIN2, HIGH);
-      digitalWrite(BIN1, LOW);
-      digitalWrite(BIN2, HIGH);
-      analogWrite(PWMA, s1);
-      analogWrite(PWMB, s2);
+      speedControl('G', 0, 0);
       Flag = 1;
     }
     //--------------------------------------------------------right vichol
@@ -148,8 +202,9 @@ void TwoBlockFollow()
       digitalWrite(BIN2, LOW);
       analogWrite(PWMB, s2);
     }
+    else analogWrite(PWMB, 0);
     //--------------------------------------------------------left vichol
-    else if (my2 < 81)
+     if (my2 < 81)
     {
       digitalWrite(AIN1, LOW);
       digitalWrite(AIN2, HIGH);
@@ -163,20 +218,13 @@ void TwoBlockFollow()
       analogWrite(PWMA, s1);
     }
     //--------------------------------------------------------go stright
-    else {
-      digitalWrite(AIN1, LOW);
-      digitalWrite(AIN2, HIGH);
-      digitalWrite(BIN1, LOW);
-      digitalWrite(BIN2, HIGH);
-      analogWrite(PWMA, s1);
-      analogWrite(PWMB, s2);
-    }
+    else analogWrite(PWMA, 0);
 
     delay (40);
   }
   else
   {
-    if ((mx2 < 105) && (mx1 > 210) && (my1 >= 81) && (my1 <= 150) && (my2 >= 81) && (my2 <= 150))
+    if (D < 10 && Area2 > 400 && Area1 > 400)
     {
       pixy.changeProg("line");
       digitalWrite(AIN1, LOW);
@@ -201,8 +249,9 @@ void TwoBlockFollow()
       digitalWrite(BIN2, LOW);
       analogWrite(PWMB, s2);
     }
+    else  analogWrite(PWMB, 0);
     //--------------------------------------------------------left vichol
-    else if (my1 < 81)
+    if (my1 < 81)
     {
       digitalWrite(AIN1, LOW);
       digitalWrite(AIN2, HIGH);
@@ -216,77 +265,18 @@ void TwoBlockFollow()
       analogWrite(PWMA, s1);
     }
     //---------------------------------------------------------go stright
-    else {
-      digitalWrite(AIN1, LOW);
-      digitalWrite(AIN2, HIGH);
-      digitalWrite(BIN1, LOW);
-      digitalWrite(BIN2, HIGH);
-      analogWrite(PWMA, s1);
-      analogWrite(PWMB, s2);
-    }
+    else analogWrite(PWMA, 0);
     delay(400);
   }
 }
 void loop() {
   // put your main code here, to run repeatedly:
-  //-----------------------------color
-
-  pixy.changeProg("line");
-
-  while (!pixy.line.getMainFeatures())
-  { pixy.changeProg("color");
-    pixy.ccc.getBlocks();
-
-    if (Flag == 1) break;
-    if (pixy.ccc.numBlocks = 1)
-    {
-      Serial.print("one block");
-
-      //---------------------------------------------------when only find one block
-      OneBlockFollow();
-      pixy.changeProg("line");
-    }
-    else if (pixy.ccc.numBlocks > 1)
-    {
-      Serial.print("two block");
-      TwoBlockFollow();
-      pixy.changeProg("line");
-    }
-    else
-    {
-      if (countTurn < 100) //---------------------------------------------need test turn speed
-      {
-        digitalWrite(AIN1, LOW);
-        digitalWrite(AIN2, HIGH);
-        digitalWrite(BIN1, HIGH);
-        digitalWrite(BIN2, LOW);
-        analogWrite(PWMA, s1);
-        analogWrite(PWMB, s2);//------------------------------------------
-        delay(30);
-        countTurn = countTurn + 10;
-        pixy.changeProg("line");//------------------------------------
-      }
-      else
-      {
-        TR();//-----------------------------------------------------------
-        pixy.changeProg("line");
-        delay(70);
-      }
-    }
-
-  }
-
-
-
-
-
-
-  //-----------------------------color
+  
 
   pixy.line.getMainFeatures();
   pixy.line.getAllFeatures();
 
-
+  voidProject();
 
 
   double  m_x0, m_x1, m_y0, m_y1, y, x;
@@ -314,23 +304,23 @@ void loop() {
   Serial.println(angle);
   if ((tana > -1.25 && tana < -0.94) || (tana > 1.45 && tana < 1.8))
   {
-    GS();
+    speedControl('G', 0, 0);
     Serial.println("GS");
     delay(300);
   }
   else if ((tana < -0.35 && tana >= -0.96) || (tana < 0.40) && (tana >= -0.04))
   {
-    TR();
+    speedControl('R', 0, 0);
     Serial.println("TR");
     delay(300);
   }
   else if ((tana < 1.3 && tana > 1.09) || (tana > 1.8) || (tana > 0.40 && tana < 0.89))
-  { TL();
+  { speedControl('L', 0, 0);
     Serial.println("TL");
     delay(300);
   }
   else {
-    GS();
+    speedControl('G', 0, 0);
     Serial.println("gs");
     delay(300);
   }
